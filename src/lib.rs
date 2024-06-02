48dffcc893cef3f79635a5bd73547c5820440c6c
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2024-06-02 19:20:20 +0200

    ✅ test: add tests for CardKanban creation and modification
    Add tests for creating a `CardKanban` and modifying its name and category.
    Includes tests for:
    - Creating a Kanban card.
    - Adding a name to a Kanban card.
    - Adding a category to a Kanban card.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,39 @@
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    #[derive(PartialEq, Debug)]
+    struct CardKanban {
+        name: String,
+        category: String,
+    }
+
+    pub fn create_card_kanban() -> CardKanban {
+        CardKanban { name: "".to_string(), category: "".to_string() }
+    }
+
+    pub fn add_name(p0: CardKanban, p1: &str) -> CardKanban {
+        CardKanban {name: p1.to_string(), category: "".to_string() }
+    }
+    pub fn add_category(p0: CardKanban, p1: &str) -> CardKanban {
+        return CardKanban { name: p0.name, category: p1.to_string()}
+    }
+    #[test]
+    fn it_should_create_a_card_kanban() {
+        let card_kanban = create_card_kanban();
+        assert_eq!(card_kanban, CardKanban { name: "".to_string(), category: "".to_string() });
+    }
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_should_create_a_card_kanban_with_name() {
+        let mut card_kanban = create_card_kanban();
+        card_kanban = add_name(card_kanban, "test");
+        assert_eq!(card_kanban, CardKanban { name: "test".to_string(), category: "".to_string() });
     }
+    #[test]
+    fn it_should_create_a_card_kanban_with_category() {
+        let mut card_kanban = create_card_kanban();
+        card_kanban = add_category(card_kanban, "test");
+        assert_eq!(card_kanban, CardKanban { name: "".to_string(), category: "test".to_string() });
+    }
+
+
 }
